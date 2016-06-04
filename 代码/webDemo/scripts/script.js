3a0da85346c0eae80010ac65f4b1e536c9d18f6b
Kimi <akaxiaok@gmail.com>
2016-06-04 21:37:13 +0800

    首页

--- 代码/webDemo/scripts/script.js
+++ 代码/webDemo/scripts/script.js
@@ -2,3 +2,16 @@
  * Created by lenovo on 2016/6/3.
  */
 'use strict';
+$(function () {
+    var $view = $('.view-point'),
+        int = setInterval(function () {
+            $view.animate({left: '20%', top: '80%'}, 8000)
+                .animate({left: '20%', top: '20%'}, 8000)
+                .animate({left: '60%', top: '80%'}, 8000)
+                .animate({left: '60%', top: '20%'}, 8000);
+        }, 0);
+    $('.close').click(function () {
+        $view.hide();
+        clearInterval(int);
+    });
+});
